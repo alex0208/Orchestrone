73e61417509e9c7c82f0509902f9f836195b8cd5
alex0208 <alexander.zahariev1996@gmail.com>
2020-05-27 18:39:19 +0200

    Throw an error when command is called on object different than Group in a function call

--- FunctionCall.py
+++ FunctionCall.py
@@ -26,6 +26,8 @@ class FunctionCall(AST):
             if assign.expr:
                 func_sym_table.append(assign.expr.fillSymbolTable())
             if assign.cmd:
+                if resolved_args_dict.get(assign.id).type != 'Group':
+                    raise Exception('Expected variable type Group but got {}'.format(resolved_args_dict.get(assign.id).type))
                 if assign.cmd.type == 'takeOff':
                     if isinstance(assign.cmd.height, str):
                         tmp_height = resolved_args_dict.get(assign.cmd.height)
